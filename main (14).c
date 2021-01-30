5ca747ba1f5514a7486b46fc4e439e6d7ed143a7
irematak <75726319+irematak@users.noreply.github.com>
2021-01-30 15:41:30 +0300

    Add files via upload

---
+++ main (14).c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+char letter;
+char *ch_arr=(char *)malloc(100*sizeof(char));
+int i,c,count=0;
+int *arr=(int *)malloc(99*sizeof(int));
+printf("Enter a text:");gets(ch_arr);
+printf("Enter a letter:");scanf("%c",&letter);
+for(i=0;i<strlen(ch_arr);i++){
+    if(letter==ch_arr[i])
+    arr[count++]=i;
+}
+printf("This text has %d '%c'character.\n",count,letter);
+for(c=0;c<count;c++)
+printf("%d. index of '%c'character : %d\n",c+1,letter,arr[c]);
+return 0;
+}
