e90db15086c6ea1a8154450c9b649fa20be5944d
irematak <75726319+irematak@users.noreply.github.com>
2021-01-30 12:18:55 +0300

    Update and rename main (9).c to ConvertSmallLetterToCapitalLetter/main.c

--- ConvertSmallLetterToCapitalLetter/main.c
+++ ConvertSmallLetterToCapitalLetter/main.c
@@ -4,7 +4,7 @@
 int main()
 {
    char letter;
-   printf("Enter a little letter:");scanf("%c",&letter);
+   printf("Enter a small letter:");scanf("%c",&letter);
    if(letter>='a'&&letter<='z')
    letter-=32;
    printf("Capital letter:%c",letter);
