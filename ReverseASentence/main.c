4deb0b1d79b16059dd40172de6b762d8f4fdb488
irematak <75726319+irematak@users.noreply.github.com>
2021-01-31 12:58:40 +0300

    Create main.c

---
+++ ReverseASentence/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+void reverseSentence(){
+char c;
+scanf("%c",&c);
+if(c!='\n'){
+    reverseSentence();
+    printf("%c",c);
+     }
+}
+
+int main()
+{
+ printf("Enter a sentence:");
+ reverseSentence();
+  return 0;
+}
