7590a53fcd7ce10a3c471aecca801f6714e37c09
irematak <75726319+irematak@users.noreply.github.com>
2021-01-31 17:26:00 +0300

    Update main.c

--- CheckTheTypeOfTriangle/main.c
+++ CheckTheTypeOfTriangle/main.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 int main()
 {
-	int side1, side2, side3;
-  	printf("\n Please Enter Three Sides of a Triangle : ");
-  	scanf("%d%d%d", &side1, &side2, &side3);
+    int side1, side2, side3;
+    printf("Please Enter Three Sides of a Triangle : ");
+    scanf("%d%d%d", &side1, &side2, &side3);
     if(side1==side2&&side1==side3&&side2==side3)
-    printf("This is an Equilateral Triangle");
+    printf("This is an Equilateral Triangle.");
     else if((side1==side2&&side1!=side3&&side2!=side3)||(side1==side3&&side1!=side2&&side2!=side3)||(side3==side2&&side1!=side3&&side2!=side1))
-    printf("This is an Isosceles Triangle");
+    printf("This is an Isosceles Triangle.");
     else
-    printf("This is a Scalene Triangle");
+    printf("This is a Scalene Triangle.");
     return 0;
 }
