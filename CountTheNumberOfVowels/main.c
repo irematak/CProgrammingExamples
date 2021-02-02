a876f4c71335f1cc10325beaf1c7668c5022261b
irematak <75726319+irematak@users.noreply.github.com>
2021-02-02 12:29:27 +0300

    Update main.c

--- CountTheNumberOfVowels/main.c
+++ CountTheNumberOfVowels/main.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 int main()
 {   int count=0;
-    char *word=(char *)malloc(20*sizeof(char));
-    printf("Enter a word:");gets(word);
-    for(int i=0;i<strlen(word);i++){
-        if (word[i] == 'a' || word[i] == 'e' || word[i] == 'i' ||
-            word[i] == 'o' || word[i] == 'u' || word[i] == 'A' ||
-            word[i] == 'E' || word[i] == 'I' || word[i] == 'O' ||
-            word[i] == 'U')
+    char *string=(char *)malloc(100*sizeof(char));
+    printf("Enter a string:");gets(string);
+    for(int i=0;i<strlen(string);i++){
+        if (string[i] == 'a' || string[i] == 'e' || string[i] == 'i' ||
+            string[i] == 'o' || string[i] == 'u' || string[i] == 'A' ||
+            string[i] == 'E' || string[i] == 'I' || string[i] == 'O' ||
+            string[i] == 'U')
             count++;
     }
-    printf("Vowels:%d",count);
+    printf("Number of vowels::%d",count);
     return 0;
 }
