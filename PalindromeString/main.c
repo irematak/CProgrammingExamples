4cb395b1984096a9ec18889a3237516227c28567
irematak <75726319+irematak@users.noreply.github.com>
2021-02-22 22:35:31 +0300

    Update main.c

--- PalindromeString/main.c
+++ PalindromeString/main.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 int main(){
-char *ch_arr=(char)malloc(10000*sizeof(char));
-char *ch_arr2=(char)malloc(10000*sizeof(char));
-int count=0;
+char *ch_arr=(char*)malloc(10000*sizeof(char));
+char *ch_arr2=(char*)malloc(10000*sizeof(char));
+int count=0,counter=0;
 printf("Enter the string:");gets(ch_arr);
-for(int i=0,j=strlen(ch_arr)-1;i<strlen(ch_arr);i++,j--){
+for(int i=0;ch_arr[i]!='\0';i++)
+    counter++;
+for(int i=0,j=counter-1;i<counter;i++,j--){
     ch_arr2[j]=ch_arr[i];
 }
-for(int i=0;i<strlen(ch_arr);i++){
+for(int i=0;i<counter;i++){
    if(ch_arr[i]==ch_arr2[i])
         count++;
 }
-if(count==strlen(ch_arr))
+if(count==counter)
     printf("String is  palindrome.");
 else
     printf("String is not palindrome.");
 return 0;
 }
+
